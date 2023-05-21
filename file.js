a9b93be65f0c8da7263b5bd5f73d48b103758ed3
Jarkus47 <131869475+Jarkus47@users.noreply.github.com>
2023-05-21 10:54:46 +0300

    Add files via upload

---
+++ file.js
@@ -0,0 +1,14 @@
+let smartphone = new Map([
+  ["apple", "green"],
+
+  ["samsung", "red"],
+
+  ["honor",    "blue"],
+
+  ["xiaomi", "white"]
+]);
+for (let elem of smartphone) { 
+
+   console.log(elem); 
+
+}
